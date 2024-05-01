6997cc91ab66e0ac4e128f7f70591ae5ba73465c
jonoya <jonoyanguren@gmail.com>
2024-05-01 20:37:44 +0200

    feat: change to vite project

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,34 @@
-import { Button, Input, Title } from "./components";
+import "./App.css";
 import { useTranslation } from "react-i18next";
+import "./i18n";
+import { Button, Input, Title } from "./components";
+import { ChangeEvent } from "react";
 
 function App() {
   const { t, i18n } = useTranslation();
+
   return (
-    <div className="App">
+    <>
       <div>
-        <button className="m-4" onClick={() => i18n.changeLanguage("en")}>
-          English
-        </button>
-        <button className="m-4" onClick={() => i18n.changeLanguage("es")}>
-          Español
-        </button>
+        <div>
+          <button className="m-2" onClick={() => i18n.changeLanguage("en")}>
+            English
+          </button>
+          <button className="m-2" onClick={() => i18n.changeLanguage("es")}>
+            Español
+          </button>
+        </div>
+        <Title>{t("title")}</Title>
+        <Input
+          name={""}
+          value={""}
+          onChange={function (event: ChangeEvent<HTMLInputElement>): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
+        <Button>Button</Button>
       </div>
-      <Title>{t("title")}</Title>
-      <Input name="name" value="value" onChange={() => {}} />
-      <Button>Button</Button>
-    </div>
+    </>
   );
 }
 
--- web/src/i18n.ts
+++ web/src/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import { es } from "./locales/es";
 import { en } from "./locales/en";
+import { es } from "./locales/es";
 
 i18n.use(initReactI18next).init({
   resources: {
