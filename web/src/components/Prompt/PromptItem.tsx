a0ae1352aa44394e4dc76f0457ab9b2a3589ec49
jonoya <jonoyanguren@gmail.com>
2024-05-05 21:45:07 +0200

    feat: fix item issues

--- web/src/components/Prompt/PromptItem.tsx
+++ web/src/components/Prompt/PromptItem.tsx
@@ -1,14 +1,16 @@
 import { Prompt } from "../../types";
 
 export const PromptItem = ({ prompt }: { prompt: Prompt }) => {
-  console.log("PROMTP", prompt);
   return (
     <div className="p-6 rounded-xl shadow-lg bg-white text-left">
       <p className="font-bold text-lg text-gray-900">{prompt.title}</p>
       <p className="text-sm my-2 underline text-gray-800">
         {prompt.createdBy.name}
       </p>
-      <p className="text-sm font-medium text-gray-500">{prompt.description}</p>
+      <p className="text-sm font-medium text-gray-500">
+        {prompt.description.slice(0, 140)}
+        {prompt.description.length > 140 ? "..." : ""}
+      </p>
       <div className="flex gap-2 mt-2 flex-wrap">
         {prompt.tags.map((tag) => (
           <div key={tag}>
@@ -19,6 +21,7 @@ export const PromptItem = ({ prompt }: { prompt: Prompt }) => {
       <div className="flex gap-2 flex-wrap my-4">
         {prompt.categories.map((category) => (
           <p
+            key={category._id}
             className="py-1 px-3 rounded-lg text-sm uppercase"
             style={{
               color: category.textColor,
@@ -31,7 +34,7 @@ export const PromptItem = ({ prompt }: { prompt: Prompt }) => {
       </div>
       <div className="flex gap-2 mt-2 justify-end">
         {prompt.platforms.map((platform) => (
-          <img className="w-8" src={platform.logo} />
+          <img key={platform._id} className="w-8" src={platform.logo} />
         ))}
       </div>
     </div>
