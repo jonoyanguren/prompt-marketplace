1322b6c0ff9d3ac5d3cae59ff0cb9cb8c1c50a62
jonoya <jonoyanguren@gmail.com>
2024-06-29 13:34:17 +0200

    feat: check user already exist

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -17,6 +17,11 @@ export const register = async (req: ExtendedRequest, res: Response) => {
   try {
     const { username, password, name, email, creator } = req.body;
 
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({
