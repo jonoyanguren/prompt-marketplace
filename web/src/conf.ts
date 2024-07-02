1ee5dcdf34caaf1c83d374a2887eb16ff24f7c3f
jonoya <jonoyanguren@gmail.com>
2024-07-02 17:00:45 +0200

    feat: url para subida

--- web/src/conf.ts
+++ web/src/conf.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:3202";
+// export const API_URL = "http://localhost:3202";
 // export const API_URL = "https://next-akita-ample.ngrok-free.app";
-// export const API_URL =
-//   "http://prompt-api-1.eba-39cnde3k.eu-west-1.elasticbeanstalk.com";
+export const API_URL =
+  "http://prompt-api-1.eba-39cnde3k.eu-west-1.elasticbeanstalk.com";
